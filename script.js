7e8e4643001aad5fe279709f61e5f4f614c0a639
Mahsood Ebrahim <mahsoodebrahim@gmail.com>
2021-10-31 21:52:15 -0700

    Created table to display books

--- script.js
+++ script.js
@@ -33,18 +33,23 @@ function showLibrary() {
   const table = document.getElementById("table");
 
   myLibrary.forEach((book) => {
+    // Create elements for book attributes and row within the table
     const title = document.createElement("td");
     const author = document.createElement("td");
     const pages = document.createElement("td");
     const read = document.createElement("td");
     const bookRow = document.createElement("tr");
 
+    // Add content for each element
     title.appendChild(document.createTextNode(book.title));
     author.appendChild(document.createTextNode(book.author));
     pages.appendChild(document.createTextNode(book.pages));
     read.appendChild(document.createTextNode(book.read));
+
+    // Append book attributes to row
     bookRow.append(title, author, pages, read);
 
+    // Append row to table
     table.appendChild(bookRow);
   });
 }
