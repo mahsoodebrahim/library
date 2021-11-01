45d47faec51b7656b2ad937ad93abe1d668cd968
Mahsood Ebrahim <mahsoodebrahim@gmail.com>
2021-11-01 16:53:25 -0700

    toggle read status of book

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ let myLibrary = [
     title: "title1",
     author: "author1",
     pages: 1,
-    read: "finished",
+    read: "Finished",
     isDisplayed: false,
     id: 34,
   },
@@ -11,7 +11,7 @@ let myLibrary = [
     title: "title2",
     author: "author2",
     pages: 2,
-    read: "not read yet",
+    read: "Unfinished",
     isDisplayed: false,
     id: 342,
   },
@@ -19,7 +19,7 @@ let myLibrary = [
     title: "title3",
     author: "author3",
     pages: 3,
-    read: "finished",
+    read: "Finished",
     isDisplayed: false,
     id: 123,
   },
@@ -27,7 +27,7 @@ let myLibrary = [
     title: "title4",
     author: "author4",
     pages: 4,
-    read: "not read yet",
+    read: "Unfinished",
     isDisplayed: false,
     id: 12,
   },
@@ -47,7 +47,7 @@ function Book(title, author, pages, read) {
 
   this.info = function () {
     return `${this.title} by ${this.author}, ${this.pages} pages, ${
-      this.read ? "Finished" : "Not yet read"
+      this.read ? "Finished" : "Unfinished"
     }`;
   };
 }
@@ -60,12 +60,14 @@ function showLibrary() {
 
     book.isDisplayed = true;
     const removeBookIcon = '<i class="fas fa-minus-circle"></i>';
+    const readStatusButton = "<button>Toggle Read Status</button>";
 
     // Create elements for book attributes and row within the table
     const title = document.createElement("td");
     const author = document.createElement("td");
     const pages = document.createElement("td");
     const read = document.createElement("td");
+    const toggleReadStatusBtn = document.createElement("td");
     const removeBookBtn = document.createElement("td");
     const bookRow = document.createElement("tr");
 
@@ -74,16 +76,29 @@ function showLibrary() {
     author.appendChild(document.createTextNode(book.author));
     pages.appendChild(document.createTextNode(book.pages));
     read.appendChild(document.createTextNode(book.read));
+    read.id = `book-${book.id}-read-status`;
+    toggleReadStatusBtn.innerHTML = readStatusButton;
     removeBookBtn.innerHTML = removeBookIcon;
     bookRow.id = book.id;
 
-    // add event listenr for removeBookBtn
+    // add event listeners
     removeBookBtn.addEventListener("click", () =>
       removeBookFromLibrary(book.id)
     );
 
+    toggleReadStatusBtn.addEventListener("click", () =>
+      changeReadStatus(read.id)
+    );
+
     // Append book attributes to row
-    bookRow.append(title, author, pages, read, removeBookBtn);
+    bookRow.append(
+      title,
+      author,
+      pages,
+      read,
+      toggleReadStatusBtn,
+      removeBookBtn
+    );
 
     // Append row to table
     table.appendChild(bookRow);
@@ -97,7 +112,7 @@ function addBookToLibrary() {
   const pages = document.getElementById("pages").value;
   const read = document.getElementById("read").checked
     ? "Finished"
-    : "Not yet read";
+    : "Unfinished";
 
   // Create a new book with user data
   const newBook = new Book(title, author, pages, read);
@@ -130,6 +145,11 @@ function removeAllChildNodes(parent) {
   while (parent.firstChild) {
     parent.removeChild(parent.firstChild);
   }
+}
+
+function changeReadStatus(readID) {
+  const read = document.getElementById(readID);
+  read.innerText = read.innerText === "Finished" ? "Unfinished" : "Finished";
 }
 
 showLibrary();
