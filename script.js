fc9e48d790c04540cbd20dd73c23e6c9aceffd1f
Mahsood Ebrahim <mahsoodebrahim@gmail.com>
2021-10-31 21:28:37 -0700

    Created Book object constructor, library storage, and function to add Book instances to library

---
+++ script.js
@@ -0,0 +1,25 @@
+const myLibrary = [];
+
+function Book(title, author, pages, read) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
+
+  this.info = function () {
+    return `${this.title} by ${this.author}, ${this.pages} pages, ${
+      this.read ? "finished" : "not read yet"
+    }`;
+  };
+}
+
+function addBookToLibrary() {
+  const title = document.getElementById("title").value;
+  const author = document.getElementById("author").value;
+  const pages = document.getElementById("pages").value;
+  const read = document.getElementById("read").value;
+
+  const newBook = new Book(title, author, pages, read);
+
+  myLibrary.push(newBook);
+}
